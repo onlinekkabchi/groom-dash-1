7ea583af42669020b053ffbdac58b504355bac74
onlinekkabchi <onlinekkabchi@gmail.com>
2023-06-16 15:22:12 +0900

    update

--- static/map/main.js
+++ static/map/main.js
@@ -1,48 +1,34 @@
 console.log("map");
-// Convert latitude and longitude to canvas coordinates
-function convertCoordinates(latitude, longitude) {
-  const x = ((longitude + 180) / 360) * 400;
-  const y = ((-latitude + 90) / 180) * 300;
-  return { x, y };
-}
 
 window.onload = function () {
-  const app = document.getElementById("app");
-  const canvas = document.createElement("canvas");
-  canvas.width = 400; // Set the width of the canvas
-  canvas.height = 300; // Set the height of the canvas
+  const app = document.querySelector("#app");
 
-  app.appendChild(canvas);
+  const container = document.querySelector("#map");
+  // container.style.width = "500px";
+  // container.style.height = "200px";
+  // container.style.paddingTop = "10px";
 
-  const ctx = canvas.getContext("2d");
+  const options = {
+    //지도를 생성할 때 필요한 기본 옵션
+    center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+    level: 3, //지도의 레벨(확대, 축소 정도)
+  };
 
-  const borderPoints = [
-    [45, -123], // Portland, Oregon (Start Point)
-    [48, -122], // Seattle, Washington
-    [48, -117], // Spokane, Washington
-    [44, -116], // Boise, Idaho
-    [41, -112], // Salt Lake City, Utah
-    [40, -105], // Denver, Colorado
-    [35, -107], // Albuquerque, New Mexico
-    [32, -106], // El Paso, Texas
-    [32, -117], // San Diego, California
-  ];
+  const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
 
-  // Define the border path for the United States
-  ctx.beginPath();
+  app.appendChild(container);
+  // 지도에 표시할 원을 생성합니다
+  var circle = new kakao.maps.Circle({
+    center: new kakao.maps.LatLng(33.450701, 126.570667), // 원의 중심좌표 입니다
+    radius: 50, // 미터 단위의 원의 반지름입니다
+    strokeWeight: 5, // 선의 두께입니다
+    strokeColor: "#75B8FA", // 선의 색깔입니다
+    strokeOpacity: 1, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+    strokeStyle: "dashed", // 선의 스타일 입니다
+    fillColor: "#CFE7FF", // 채우기 색깔입니다
+    fillOpacity: 0.7, // 채우기 불투명도 입니다
+  });
 
-  // Move to the starting point
-  const startPoint = convertCoordinates(borderPoints[0][0], borderPoints[0][1]);
-  ctx.moveTo(startPoint.x, startPoint.y);
-
-  // Draw the border path
-  for (let i = 1; i < borderPoints.length; i++) {
-    const { x, y } = convertCoordinates(borderPoints[i][0], borderPoints[i][1]);
-    ctx.lineTo(x, y);
-  }
-  // Set the stroke style
-  ctx.strokeStyle = "red";
-
-  // Stroke the border path
-  ctx.stroke();
+  // 지도에 원을 표시합니다
+  circle.setMap(map);
 };
